0a56ea1deea16ddd62b7fc6f7162b118d6d9718d
jamilsonh <2jamilson@gmail.com>
2024-01-14 15:59:23 -0300

    feat: styling the menu component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,15 +5,37 @@ import {
   ContainerLeft,
   ContainerMiddle,
   ContainerRight,
+  MobileMenuButton,
 } from './styles';
 import Image from 'next/image';
 
 export default function Header() {
   return (
     <Container>
+      <MobileMenuButton>
+        <Image
+          src='icon-menu.svg'
+          width={16}
+          height={15}
+          alt='logo'
+          sizes='100vw'
+          onClick={() => setMenuIsVisible(true)}
+        />
+      </MobileMenuButton>
       <ContainerLeft>
-        <Image src='logo.svg' width='200' height='50' alt='logo' />
+        <Image
+          width={138}
+          height={20}
+          src='logo.svg'
+          alt='logo'
+          sizes='100vw'
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+        />
       </ContainerLeft>
+
       <ContainerMiddle>
         <a>Collections</a>
         <a>Men</a>
--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -2,13 +2,32 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 90%;
-  border: 1px solid black;
+  border-bottom: 1px solid black;
   margin: 0 auto;
   height: 150px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 0 10px;
+
+  @media (max-width: 768px) {
+    border-bottom: none;
+  }
+`;
+
+export const MobileMenuButton = styled.div`
+  display: none;
+  text-decoration: none;
+  border: none;
+  background-color: transparent;
+
+  @media (max-width: 768px) {
+    display: block;
+    text-decoration: none;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+  }
 `;
 
 export const ContainerMiddle = styled.nav`
@@ -16,13 +35,37 @@ export const ContainerMiddle = styled.nav`
   border: 1px solid black;
   gap: 30px;
   display: flex;
+  align-items: center;
+  position: relative;
+  height: 100px;
 
   a {
+    position: relative; /* Isso permite posicionar o pseudo-elemento em relação a este link */
     text-decoration: none;
-    &:hover {
-      background: #555;
-      cursor: pointer;
+    cursor: pointer;
+
+    &::after {
+      content: '';
+      position: absolute;
+      left: 0;
+      bottom: -60px; /* Ajuste esta distância para controlar o quão longe a borda está do item do menu */
+      width: 100%;
+      height: 5px; /* Altura da borda */
+      background-color: transparent; /* Cor de fundo inicial (transparente) */
+      transition: background-color 0.3s; /* Animação suave para mudança de cor */
     }
+
+    &:hover::after {
+      background-color: black; /* A cor da borda que aparecerá ao passar o mouse */
+    }
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
   }
 `;
 
@@ -32,7 +75,8 @@ export const ContainerLeft = styled.div`
 `;
 
 export const ContainerRight = styled.div`
-  width: 150px;
+  width: 20%;
+  max-width: 150px;
   border: 1px solid black;
   display: flex;
   justify-content: space-around;
