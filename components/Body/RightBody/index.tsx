66e1ebb8adbb19da5c3f59a3a87633d092294920
jamilsonh <2jamilson@gmail.com>
2024-01-17 19:50:08 -0300

    feat: body rightpart adjust

--- components/Body/RightBody/index.tsx
+++ components/Body/RightBody/index.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, ContainerPrice } from './styles';
 
 export default function RightBody() {
-  return <Container>index</Container>;
+  return (
+    <Container>
+      <h3>SNEAKER COMPANY</h3>
+      <h1>Fall Limited Edition Sneakers</h1>
+      <p>
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they'll withstand everything the
+        weather can offer.
+      </p>
+      <ContainerPrice>
+        <h2>$125.00</h2>
+        <h3>50%</h3>
+      </ContainerPrice>
+      <h4>$250.00</h4>
+    </Container>
+  );
 }
--- components/Body/RightBody/styles.ts
+++ components/Body/RightBody/styles.ts
@@ -3,4 +3,45 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 45%;
   border: 3px solid black;
+  display: flex;
+  flex-direction: column;
+  min-height: 300px;
+  justify-content: space-between;
+
+  h3 {
+    font-size: 1rem;
+    color: hsl(26, 100%, 55%);
+    letter-spacing: 1px;
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  p {
+    font-weight: 400;
+    line-height: 1.5rem;
+    color: hsl(219, 9%, 45%);
+  }
+
+  h4 {
+    color: hsl(219, 9%, 45%);
+    font-weight: 400;
+    text-decoration: line-through;
+  }
+`;
+
+export const ContainerPrice = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  h3 {
+    font-size: 1rem;
+    color: hsl(26, 100%, 55%);
+    letter-spacing: 1px;
+    background-color: hsla(26, 100%, 55%, 0.158);
+    padding: 0.2rem 0.5rem;
+    border-radius: 5px;
+  }
 `;
